af9ee5c3d308fa63a601b547b86585fe46d2574d
fionaliu66 <fiona.liu0606@gmail.com>
2024-02-27 15:19:29 +0100

    Update Program.cs

--- Uppgift1_Jiangyi/Program.cs
+++ Uppgift1_Jiangyi/Program.cs
@@ -4,7 +4,87 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ask for employ input
+            Dictionary<string, int> employList = new Dictionary<string, int>();
+            ProgramOptions();
+
+            void ProgramOptions()
+            {
+                Console.WriteLine("What whould you like to do today");
+                Console.WriteLine("1: Resgiter new employ");
+                Console.WriteLine("2: Print out all members");
+                Console.WriteLine("3: Close app");
+                string choice = Console.ReadLine() ?? "";
+
+                int number;
+                //saft convert string to int
+                if(int.TryParse(choice, out number))
+                {
+                    if(number >= 1 && number <= 3)
+                    {
+                        switch(number)
+                        {
+                            case 1:
+                                SignUp();
+                                break;
+                            case 2:
+                                ShowUp();
+                                break;
+                            case 3:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose between 1-3");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input is not a valid integer");
+                }
+            }//end of Program Options
+
+            void SignUp()
+            {
+                Console.WriteLine("Name of the employ");
+                string name = Console.ReadLine();
+                if(name != null)
+                {
+                    Console.WriteLine("Salary of the employ");
+                    int salary;
+                    if(int.TryParse(Console.ReadLine(), out salary))
+                    {
+                        //successfully convert string to int
+                        //store it to employList
+                        employList.Add(name, salary);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Write in the right salary");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Name cannot be Null");
+                }
+                ProgramOptions();
+
+            }// end of Sighup
+
+            void ShowUp()
+            {
+                if(employList.Count > 0)
+                {
+                    foreach(var employ in employList)
+                    {
+                        Console.WriteLine($"Name: {employ.Key}, Salary: {employ.Value}");
+                    }
+                }
+                ProgramOptions();
+            } // end of ShowUp
+
         }
     }
 }
